1043fa387699d8886e6683ef54d95b5c757f9455
dranswal <dranswal@insightresults.com>
2019-11-07 12:53:30 +0530

    header component tested

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,14 +4,14 @@ import Logo from '../../assets/Logo/Logo.jpg';
 
 const Header = (props) => {
   return (
-    <header>
+    <header data-test='headerComponent'>
       <div className='wrap'>
         <div className='logo'>
-          <img src={Logo} alt='Logo' />
+          <img data-test='logoComponent' src={Logo} alt='Logo' />
         </div>
       </div>
     </header>
   )
 }
 
-export default Header
+export default Header;
